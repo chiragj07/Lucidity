a515236ca803c5fe1525381a2707c14adcf57545
chiragj07 <joshi.chirag54@gmail.com>
2024-04-28 11:48:33 +0530

    validations added

--- src/Components/Modal.tsx
+++ src/Components/Modal.tsx
@@ -11,6 +11,9 @@ interface InventoryInterface{
   isDisabled: boolean
 }
 
+function isValid(str) {
+    return /^\d+$/.test(str);
+}
 
 
 const Modal = ({setShowModal,inventoryData, setInvetoryData, showModal} : {showModal: { state: boolean, data:InventoryInterface} , setShowModal: ({state, data}: {state: false, data: InventoryInterface }) => void, setInvetoryData: (data: InventoryInterface[]) => void, inventoryData: InventoryInterface[] } ) => {
@@ -20,10 +23,26 @@ const Modal = ({setShowModal,inventoryData, setInvetoryData, showModal} : {showM
   const [quantity, setQuantity]= useState(0)
   const [value, setValue]= useState("")
 
-  
+  const handleClose = () => {
+	setError("")
+	setShowModal({state: false, data: {}})
+	
+  }
+
+
+  const [error, setError] = useState(""); 
 
   const handleSaveData = () => {
      const {data} = showModal;
+     if (!isValid(price)) {
+	setError("Price can only be a number")
+	return
+      }
+     if (!isValid(value)) {
+	setError("Value can only be a number")
+	return
+      }
+     
      const newInventory = inventoryData.map((item) => item.name !== data.name ? item : {...item, 
       price: price === "0" ? "0" : `$${price}`,
       value: value === "0" ? "0" : `$${value}`,
@@ -49,17 +68,22 @@ const Modal = ({setShowModal,inventoryData, setInvetoryData, showModal} : {showM
       <div className="modal-content">
         <div className="modal-header">
           <h2>{"Edit Product"}</h2>
-          <CloseIcon onClick={() => setShowModal({state: false, data: {}})} style={{color: "#CEED51", cursor: "pointer"}} />
+          <CloseIcon onClick={handleClose} style={{color: "#CEED51", cursor: "pointer"}} />
         </div>
         <span>{showModal.data?.name}</span>
         <div className="input-field-containers">
           <InputField label="Category" type="text" placeholder="Category" value={category} onChange={(e: any) => setCategory(e.target.value) } />
-          <InputField label="Price" type="text" placeholder="Price" value={price} onChange={(e: any) => setPrice(e.target.value) }/>
+          <InputField label="Price" type="text" placeholder="Price" value={price} onChange={(e: any) => {
+		setError("");	
+		setPrice(e.target.value)} }/>
           <InputField label="Quantity" type="number" placeholder="Quantity" value={String(quantity)} onChange={(e: any) => setQuantity(e.target.value) }/>
-          <InputField label="Value" type="text" placeholder="Value" value={value} onChange={(e: any) => setValue(e.target.value) } />
+          <InputField label="Value" type="text" placeholder="Value" value={value} onChange={(e: any) => {
+		setError("")
+		setValue(e.target.value)} } />
         </div>
+        <span style={{color: "#EA3323"}}> {error} </span>
         <div className="modal-actions">
-          <p onClick={() => setShowModal({state: false, data: {}})}>{"Cancel"}</p>
+          <p onClick={handleClose}>{"Cancel"}</p>
           <button className="save-button" onClick={handleSaveData}>{"Save"}</button>
         </div>
       </div>
